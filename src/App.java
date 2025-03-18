84b3077405e84b031d14c0fe6579cc5400d74ef2
Unwary <samaelcasta117@gmail.com>
2025-03-18 08:36:59 -0700

    18 03 2025 4

--- src/App.java
+++ src/App.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String[] opcionesMenu = {"opcion1", "opcion2", "opcion3", "opcion4"}; int opcionUsuario = 0; Scanner input = new Scanner(System.in);
+        String[] opcionesMenu = {"opcion1", "opcion2", "opcion3", "opcion4"}; int opcionUsuario = 0; Scanner input = new Scanner(System.in); //definir variables y scanner
 
+        //*visual para terminal
         System.out.println("----------------------------------------------------------------");
         System.out.println("SISTEMA GESTION AUTOMOVILES | Universidad Tecnologica de Tijuana");
         System.out.println("----------------------------------------------------------------");
         System.out.println("MENU OPCIONES|DIGITE OPCION NUMERICA");
+        System.out.println("----------------------------------------------------------------");
 
-        for(int i = 0; i < opcionesMenu.length; i++) System.out.println((i+1) + " -> " + opcionesMenu[i]);
-        
+        for(int i = 0; i < opcionesMenu.length; i++) System.out.println((i+1) + " -> " + opcionesMenu[i]); System.out.println("----------------------------------------------------------------");
+        System.out.print("->"); opcionUsuario = input.nextInt();
+
+        switch(opcionUsuario){} //switch para todos los posibles casos
+
+
+        input.close();
     }
 }
