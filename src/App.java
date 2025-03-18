339a7e53118d684d7cc720b4a4b1a7964c072e6e
UnwaryCoder <samaelcasta117@gmail.com>
2025-03-18 06:42:59 -0700

    Commit 18 03 2025 3

--- src/App.java
+++ src/App.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        String[] opcionesMenu = {"opcion1", "opcion2", "opcion3", "opcion4"}; int opcionUsuario = 0; Scanner input = new Scanner(System.in);
+
         System.out.println("----------------------------------------------------------------");
         System.out.println("SISTEMA GESTION AUTOMOVILES | Universidad Tecnologica de Tijuana");
         System.out.println("----------------------------------------------------------------");
         System.out.println("MENU OPCIONES|DIGITE OPCION NUMERICA");
+
+        for(int i = 0; i < opcionesMenu.length; i++) System.out.println((i+1) + " -> " + opcionesMenu[i]);
+        
     }
 }
