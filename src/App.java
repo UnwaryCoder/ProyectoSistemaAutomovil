5cf7fc5cdf05eb05fdc3957e8b266c6e2482ad8c
Unwary <samaelcasta117@gmail.com>
2025-03-20 10:53:41 -0700

    Commit 20 03 2025 1

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String[] opcionesMenu = {"opcion1", "opcion2", "opcion3", "opcion4"}; int opcionUsuario = 0; Scanner input = new Scanner(System.in); //definir variables y scanner
+        String[] opcionesMenu = {"CATALOGO DE VEHICULOS.", "REALIZAR VENTA DE VEHICULOS.", "MANUAL DE USUARIO", "CREAR PRESUPUESTO", "SALIR"}; int opcionUsuario = 0; Scanner input = new Scanner(System.in); //definir variables y scanner
 
         //*visual para terminal
         System.out.println("----------------------------------------------------------------");
@@ -11,10 +11,30 @@ public class App {
         System.out.println("MENU OPCIONES|DIGITE OPCION NUMERICA");
         System.out.println("----------------------------------------------------------------");
 
-        for(int i = 0; i < opcionesMenu.length; i++) System.out.println((i+1) + " -> " + opcionesMenu[i]); System.out.println("----------------------------------------------------------------");
-        System.out.print("->"); opcionUsuario = input.nextInt();
+        for(int i = 0; i <opcionesMenu.length; i++){
+            System.out.println((i+1) + " -> " + opcionesMenu[i]);
+        }
 
-        switch(opcionUsuario){} //switch para todos los posibles casos
+         System.out.println("----------------------------------------------------------------");
+        System.out.print("->"); opcionUsuario = input.nextInt();
+        System.out.println();
+
+        switch(opcionUsuario){
+            case 1:
+            String[][] opciones = {
+                {"1", "VEHICULOS GAMA ALTA"},
+                {"2", "VEHICULOS GAMA MEDIA"},
+                {"3", "VEHICULOS GAMA BAJA"}
+            };
+
+
+            System.out.println("----------------------------------------------------------------");
+            System.out.println("SISTEMA GESTION AUTOMOVILES | Universidad Tecnologica de Tijuana");
+            System.out.println("----------------------------------------------------------------");
+            System.out.println("1. CATALOGO DE VEHICULOS | DIGITE OPCION NUMERICA");
+            System.out.println("----------------------------------------------------------------");
+            break;
+        } //switch para todos los posibles casos
 
 
         input.close(); //cerrando objeto tipo Scanner.
