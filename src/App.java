d069a2c9536a1a7ba49baa0bfd0fd334b992adc6
UnwaryCoder <samaelcasta117@gmail.com>
2025-03-31 02:59:43 -0700

    Commit 3 31 2025, terminados menu 1, gama 1,2,3

--- src/App.java
+++ src/App.java
@@ -4,8 +4,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 
-        //definir variables, vectores, matriz principal que contiene todos los vehiculos.
-        String[] opcionesMenu = {"CATALOGO DE VEHICULOS.", "REALIZAR VENTA DE VEHICULOS.", "MANUAL DE USUARIO", "CREAR PRESUPUESTO", "SALIR"}; int opcionUsuario = 0; Scanner input = new Scanner(System.in); //definir variables y scanner
+        //! definir variables, vectores, matriz principal que contiene todos los vehiculos.
+        String[] opcionesMenuPrincipal = {"CATALOGO DE VEHICULOS.", "REALIZAR VENTA DE VEHICULOS.", "MANUAL DE USUARIO", "SALIR"}; int opcionUsuario = 0; Scanner input = new Scanner(System.in); //definir variables y scanner
         String[][] listaVehiculos = {
             {"Toyota", "Corolla", "Media", "Carro Sencillo", "4"},
             {"Ford", "F-150", "Alta", "Camioneta", "6"},
@@ -38,29 +38,30 @@ public class App {
             {"Volkswagen", "Tiguan", "Media", "SUV Compacto", "4"},
             {"Kia", "Rio", "Baja", "Carro Sencillo", "4"}
         };
-        String[] opcionesDeGama = {"VEHICULOS GAMA ALTA", "VEHICULOS GAMA MEDIA", "VEHICULOS GAMA BAJA", "MOSTRAR TODOS LOS VEHICULOS", "REGRESAR"};
-        String[] opcionesGamaAlta = {"MOSTRAR TODOS LOS VEHICULOS DE GAMA ALTA", "MOSTRAR POR TIPO DE VEHICULOS", "MOSTRAR POR CANTIDAD DE CILINDROS", "REGRESAR"};
-        int opcionMenu1 = 0, opcionMenuGamaAlta = 0;
+        String[] opcionesMenu1_1 = {"MOSTRAR VEHICULOS POR GAMA", "MOSTRAR VEHICULOS POR CATEGORIA", "MOSTRAR VEHICULOS POR CILINDRAJE", "MOSTRAR TODOS LOS VEHICULOS", "REGRESAR"};
+        String[] opcionesMenu1_2 = {"MOSTRAR TODOS LOS VEHICULOS DE GAMA ALTA", "MOSTRAR TODOS LOS VEHICULOS DE GAMA MEDIA", "MOSTRAR TODOS LOS VEHICULOS DE GAMA BAJA", "REGRESAR"};
+        int opcionMenu1_1 = 0, opcionMenuGamaAlta = 0;
 
-        //*texto con aspecto visual para terminal
+        //! texto con aspecto visual para terminal, se va a repetir bastante para cada ventana del programa...
         System.out.println("----------------------------------------------------------------");
         System.out.println("SISTEMA GESTION AUTOMOVILES | Universidad Tecnologica de Tijuana");
         System.out.println("----------------------------------------------------------------");
         System.out.println("MENU OPCIONES|DIGITE OPCION NUMERICA");
         System.out.println("----------------------------------------------------------------");
 
-        //?imprimir el vector con las opciones del menu principal
-        for(int i = 0; i <opcionesMenu.length; i++){
-            System.out.println((i+1) + " -> " + opcionesMenu[i]);
+        //? imprimir el vector con las opciones del menu principal, las opciones de las ventanas, se representan en vectores para facilitar su manejo.
+        //? de modo que si hay modificaciones, no afecta el ciclo, el codigo se adapta a las modificaciones.
+        for(int i = 0; i <opcionesMenuPrincipal.length; i++){
+            System.out.println((i+1) + " -> " + opcionesMenuPrincipal[i]);
         }
 
         System.out.println("----------------------------------------------------------------");
         System.out.print("->"); opcionUsuario = input.nextInt();
 
-        //!switch del menu principal
+        //! switch del menu principal
         switch(opcionUsuario){
             case 1:
-                //? Catalogo de autos -> tipos de gama -> Cilindraje -> tipoDeVehiculo
+                //? 1. Catalogo de autos -> tipos de gama -> Cilindraje -> tipoDeVehiculo
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 
                 System.out.println("----------------------------------------------------------------");
@@ -69,70 +70,185 @@ public class App {
                 System.out.println("1. CATALOGO DE VEHICULOS | DIGITE OPCION NUMERICA");
                 System.out.println("----------------------------------------------------------------");
 
-                for(int i = 0; i<opcionesDeGama.length; i++){
-                    System.out.println((i + 1) + " -> " + opcionesDeGama[i]);
+                for(int i = 0; i<opcionesMenu1_1.length; i++){
+                    System.out.println((i + 1) + " -> " + opcionesMenu1_1[i]);
                 }
 
                 System.out.println("----------------------------------------------------------------");
-                System.out.print("->"); opcionMenu1 = input.nextInt();
+                System.out.print("->"); opcionMenu1_1 = input.nextInt(); //? leemos la opcion del menu 1.1
 
-                //?submenu de catalogo
-                switch(opcionMenu1){ //?desplegamos las opciones de gamas(baja, media, alta)
-                    case 1:
+                //! switch para menu 1.1
+                switch(opcionMenu1_1){ //?desplegamos las opciones de catalogo (mostrar vehiculos por gama, categoria, cilindraje, todos los vehiculos o regresar)
+                    case 1: //? mostrar vehiculos por gama...
 
                         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
                         System.out.println("----------------------------------------------------------------");
                         System.out.println("SISTEMA GESTION AUTOMOVILES | Universidad Tecnologica de Tijuana");
                         System.out.println("----------------------------------------------------------------");
-                        System.out.println("1. MENU CATALOGO GAMA ALTA | DIGITE OPCION NUMERICA");
+                        System.out.println("1. MENU CATALOGO | DIGITE OPCION NUMERICA");
                         System.out.println("----------------------------------------------------------------");
 
-                        for(int i = 0; i<opcionesGamaAlta.length; i++){
-                            System.out.println((i+1) + " -> " + opcionesGamaAlta[i]);
+                        for(int i = 0; i<opcionesMenu1_2.length; i++){
+                            System.out.println((i+1) + " -> " + opcionesMenu1_2[i]);
                         }
 
                         System.out.println("----------------------------------------------------------------");
-                        System.out.println("->"); opcionMenuGamaAlta = input.nextInt();
+                        System.out.print("->"); opcionMenuGamaAlta = input.nextInt();
 
+                        //! switch para menu 1.2 filtrado por gama.
                         switch(opcionMenuGamaAlta){
-                            case 1:
-                                //mostrar todos los vehiculos de gama alta...
+                            case 1: //? mostrar todos los vehiculos de gama alta...
+
                                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.println("SISTEMA GESTION AUTOMOVILES | Universidad Tecnologica de Tijuana");
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.println("MOSTRANDO VEHICULOS GAMA ALTA | DIGITE OPCION NUMERICA"); //1System.out.println();
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.println("| ID  | Marca            | Modelo           | Gama    | Tipo                | Cilindraje/Tipo Motor |");
+                                System.out.println("|---------------------------------------------------------------------------------------------------|");
+
+                                //? imprimir todos los vehiculos gama alta...
+                                int contador = 1; // Inicializa el contador de filas
+                                for (int i = 0; i < listaVehiculos.length; i++) {
+                                    if (listaVehiculos[i][2].equals("Alta")){
+
+                                        //? imprimir por secciones, tabulaciones para dar formato de tabla, ver archivo notas1.txt
+                                        System.out.printf("| %-3d | %-15s | %-15s | %-7s | %-20s | %-22s |\n",
+                                                          contador,
+                                                          listaVehiculos[i][0], // Marca
+                                                          listaVehiculos[i][1], // Modelo
+                                                          listaVehiculos[i][2], // Gama
+                                                          listaVehiculos[i][3], // Tipo
+                                                          listaVehiculos[i][4]); // Cilindraje o Tipo Motor
+                                        contador++;
+
+                                    }
+                                }
+
+                                System.out.println("|---------------------------------------------------------------------------------------------------|");
+                                System.out.println("\nPRESIONE ENTER PARA CONTINUAR");
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.print("->"); // DESPUES IMPLEMENTAR EL DO WHILE PARA QUE AQUI CIERRE...
+
+                            break;
+
+                            case 2: //? mostrar todos los vehiculos de gama media...
+
+                                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.println("SISTEMA GESTION AUTOMOVILES | Universidad Tecnologica de Tijuana");
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.println("MOSTRANDO VEHICULOS GAMA MEDIA | DIGITE OPCION NUMERICA"); //1System.out.println();
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.println("| ID  | Marca            | Modelo           | Gama    | Tipo                | Cilindraje/Tipo Motor |");
+                                System.out.println("|---------------------------------------------------------------------------------------------------|");
+
+                                //? imprimir todos los vehiculos gama media...
+                                int contador2 = 1; // Inicializa el contador de filas
+                                for (int i = 0; i < listaVehiculos.length; i++) {
+                                    if (listaVehiculos[i][2].equals("Media")){
+
+                                        //? imprimir por secciones, tabulaciones para dar formato de tabla, ver archivo notas1.txt
+                                        System.out.printf("| %-3d | %-15s | %-15s | %-7s | %-20s | %-22s |\n",
+                                                          contador2,
+                                                          listaVehiculos[i][0], // Marca
+                                                          listaVehiculos[i][1], // Modelo
+                                                          listaVehiculos[i][2], // Gama
+                                                          listaVehiculos[i][3], // Tipo
+                                                          listaVehiculos[i][4]); // Cilindraje o Tipo Motor
+                                        contador2++;
+
+                                    }
+                                }
+                                System.out.println("|---------------------------------------------------------------------------------------------------|");
+                                System.out.println("\nPRESIONE ENTER PARA CONTINUAR");
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.print("->"); // DESPUES IMPLEMENTAR EL DO WHILE PARA QUE AQUI CIERRE...
+
+                            break;
+
+                            case 3: //? mostrar todos los vehiculos de gama baja...
+
+                                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.println("SISTEMA GESTION AUTOMOVILES | Universidad Tecnologica de Tijuana");
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.println("MOSTRANDO VEHICULOS GAMA BAJA | DIGITE OPCION NUMERICA"); //1System.out.println();
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.println("| ID  | Marca            | Modelo           | Gama    | Tipo                | Cilindraje/Tipo Motor |");
+                                System.out.println("|---------------------------------------------------------------------------------------------------|");
+
+                                //? imprimir todos los vehiculos gama baja...
+                                int contador3 = 1; // Inicializa el contador de filas
+                                for (int i = 0; i < listaVehiculos.length; i++) {
+                                    if (listaVehiculos[i][2].equals("Baja")){
+
+                                        //? imprimir por secciones, tabulaciones para dar formato de tabla, ver archivo notas1.txt
+                                        System.out.printf("| %-3d | %-15s | %-15s | %-7s | %-20s | %-22s |\n",
+                                                          contador3,
+                                                          listaVehiculos[i][0], // Marca
+                                                          listaVehiculos[i][1], // Modelo
+                                                          listaVehiculos[i][2], // Gama
+                                                          listaVehiculos[i][3], // Tipo
+                                                          listaVehiculos[i][4]); // Cilindraje o Tipo Motor
+                                        contador3++;
+
+                                    }
+                                }
+
+                                System.out.println("|---------------------------------------------------------------------------------------------------|");
+                                System.out.println("\nPRESIONE ENTER PARA CONTINUAR");
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.print("->"); // DESPUES IMPLEMENTAR EL DO WHILE PARA QUE AQUI CIERRE...
+
+                            break;
+                            case 4: //regresar al menu anterior...
+
+                                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); // Limpia la pantalla
 
                                 System.out.println("----------------------------------------------------------------");
                                 System.out.println("SISTEMA GESTION AUTOMOVILES | Universidad Tecnologica de Tijuana");
                                 System.out.println("----------------------------------------------------------------");
-                                System.out.println("MOSTRANDO VEHICULOS GAMA ALTA ");
+                                System.out.println("1.1 REGRESAR AL MENU ANTERIOR | SEPA LA BOLA QUE PONER AQUI");
                                 System.out.println("----------------------------------------------------------------");
+                                System.out.println("\nPRESIONE ENTER PARA CONTINUAR...");
+                                System.out.println("----------------------------------------------------------------");
+                                System.out.print("->");
 
-                                //imprimir todos los vehiculos gama alta...
-                                for(int i = 0; i < listaVehiculos.length; i++){
+                            break;
 
-                                    if(listaVehiculos[i][2].equals("Alta")){
+                            default: //? si el usuario no selecciona una opcion valida, se le indica que la opcion no es correcta.
 
-                                        for(int j = 0; j < listaVehiculos[i].length; j++){
+                                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 
-                                            System.out.print(listaVehiculos[i][j] + " | ");
-                                        }
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.println("SISTEMA GESTION AUTOMOVILES | Universidad Tecnologica de Tijuana");
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.println("OPCION NO VALIDA, PRESIONE ENTER PARA CONTINUAR"); //1System.out.println();
+                                System.out.println("-----------------------------------------------------------------------------------------------------");
+                                System.out.print("->"); // DESPUES IMPLEMENTAR EL DO WHILE PARA QUE AQUI CIERRE...
 
-                                        System.out.println();
-                                    }
-                                }
-                                break;
-
-                            case 2: break;
-                            case 3: break;
-                            case 4: break;
-                            default: break;
+                            break;
                         }
-                        break;
-                    case 2: break;
+
+                    break;
+
+                    case 2: //? mostrar vehiculos por categoria...
+
+                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+
+                    break;
                     case 3: break;
                     case 4: break;
                     default: break;
                 }
 
-                break;
+            break;
         } //switch para todos los posibles casos
 
 
